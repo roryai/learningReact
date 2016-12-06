0d86023989b9c5133de819a1a9d946194e565cf7
Rory Aitchison <rory.aitchison@gmail.com>
2016-12-06 12:04:58 +0000

    got rid of export App

--- src/App.js
+++ src/App.js
@@ -2,58 +2,45 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './App.css';
 
-const App = () => {
-  // ReactDOM.render(
-  //   element2,
-  //   document.getElementById('root')
-  // );
-
-  ReactDOM.render(
-    setInterval(tick, 1000),
-    document.getElementById('root')
-  );
 
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
 };
 
-function formatName(user) {
-  return user.firstName + " " + user.lastName;
+const welcomeName = <Welcome name="Rory" />;
+
+function Clock(props) {
+  return (
+    <div>
+      <h1>The time is {props.date.toLocaleTimeString()}</h1>;
+    </div>
+  )
 };
-
-const user = {
-  firstName: 'Rory',
-  lastName: 'Aitchison'
-};
-
-const element = (
-  <h1 className="App">
-    Hello, {formatName(user)}!
-  </h1>
-);
-
-const element2 = (
-  <div className="App">Hello World!</div>
-);
-
-
 
 function tick() {
-  const clock = (
-    <div>
-      <h1>The time, yo</h1>
-      <h2> The time now is {new Date().toLocaleTimeString()}</h2>
-    </div>
-  );
   ReactDOM.render(
-    clock,
+    <Clock date={new Date()} />,
     document.getElementById('root')
   );
-};
+}
 setInterval(tick, 1000)
 
-ReactDOM.render(
-  element,
-  document.getElementById('root')
-);
+// function tick() {
+//   const clock = (
+//     <div>
+//       <h1>The time, yo</h1>
+//       <h2> The time now is {new Date().toLocaleTimeString()}</h2>
+//     </div>
+//   );
+//   ReactDOM.render(
+//     clock,
+//     document.getElementById('root')
+//   );
+// };
 
-export default App;
+
+// const App = () => {
+
+// };
+// export default App;
